e31e3a17557f2d596968304a228b314aead8de5b
yusufceylan <ysf.cyln@yandex.com>
2021-05-08 15:33:35 +0300

    Initial commit

---
+++ app/src/main/java/com/ysfcyln/jetpackcomposepokedex/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ysfcyln.jetpackcomposepokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ysfcyln.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposePokedexTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposePokedexTheme {
+        Greeting("Android")
+    }
+}
